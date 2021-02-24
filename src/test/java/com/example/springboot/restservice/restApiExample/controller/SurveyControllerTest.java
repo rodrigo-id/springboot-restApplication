7f0b7e7807a385e48c3273ec9623e786a221570c
Rodrigo adán Miranda santibáñez <rodrigo.miranda_ex@scotiabank.cl>
2021-02-24 12:36:58 -0300

    First examples with integration test

--- src/test/java/com/example/springboot/restservice/restApiExample/controller/SurveyControllerTest.java
+++ src/test/java/com/example/springboot/restservice/restApiExample/controller/SurveyControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
